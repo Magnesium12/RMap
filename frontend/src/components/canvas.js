fef7fb0254442b1eac8a620ddbbcb70f84800464
Magnesium12 <99383854+Magnesium12@users.noreply.github.com>
2022-11-09 12:33:24 +0530

    Update canvas.js

--- frontend/src/components/canvas.js
+++ frontend/src/components/canvas.js
@@ -9,11 +9,6 @@ const Canvas = ({draw, height, width, canvas_click}) => {
     const [scale,setScale] = useState(1);
 
     useEffect(() => {
-        // const context1 = canvas_background.current.getContext('2d');
-        // context1.scale(scale,scale)
-        // background(context1, height, width);
-        // const context2 = canvas.current.getContext('2d');
-        // context2.scale(scale,scale)
         draw(canvas, height, width);
     });
     const send_loc = (event)=>{
@@ -22,13 +17,6 @@ const Canvas = ({draw, height, width, canvas_click}) => {
         let y = event.clientY - rect.top
         canvas_click(x,y)
     }
-    const zoom_in = ()=>{
-        //setScale(current=>current+1)
-    }
-    const zoom_out = ()=>{
-        //setScale(current=>current+1)
-    }
-
     return (
         <>
             <canvas
